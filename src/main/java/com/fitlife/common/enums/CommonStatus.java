cf844a4cd35f0f8c5c618b4764127f5250226b7f
JOKERJACK0204 <captainjackngominhducjj020406@gmail.com>
2026-07-01 13:12:59 +0700

    Add exception ErrorCode

---
+++ src/main/java/com/fitlife/common/enums/CommonStatus.java
@@ -0,0 +1,6 @@
+package com.fitlife.common.enums;
+
+public enum CommonStatus {
+    ACTIVE,
+    INACTIVE
+}
--- src/main/java/com/fitlife/common/exception/ErrorCode.java
+++ src/main/java/com/fitlife/common/exception/ErrorCode.java
@@ -96,6 +96,36 @@ public enum ErrorCode {
             HttpStatus.BAD_REQUEST
     ),
 
+    DURATION_NOT_FOUND(
+            7003,
+            "Package duration not found",
+            HttpStatus.NOT_FOUND
+    ),
+
+    DURATION_CODE_ALREADY_EXISTS(
+            7004,
+            "Package duration code already exists",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    PACKAGE_INACTIVE(
+            7005,
+            "Gym package is inactive",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    DURATION_INACTIVE(
+            7006,
+            "Package duration is inactive",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    SUBSCRIPTION_NOT_FOUND(
+            7007,
+            "Subscription not found",
+            HttpStatus.NOT_FOUND
+    ),
+
     // Role errors
     ROLE_NOT_FOUND(
             4001,
